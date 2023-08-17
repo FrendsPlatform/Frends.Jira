83dfca1106118a7cade674d2980917bc4c2357be
ttossavainen <teemu.tossavainen@hiq.fi>
2023-08-17 10:50:01 +0300

    init

---
+++ Frends.Jira.DeleteIssue/Frends.Jira.DeleteIssue/Definitions/Input.cs
@@ -0,0 +1,13 @@
+﻿namespace Frends.Jira.DeleteIssue.Definitions;
+
+/// <summary>
+/// Input parameters.
+/// </summary>
+public class Input
+{
+    /// <summary>
+    /// Specify the issue key or ID you want to delete.
+    /// </summary>
+    /// <example>TT-1, 123</example>
+    public string IdOrKey { get; set; }
+}
---
+++ Frends.Jira.DeleteIssue/Frends.Jira.DeleteIssue/Definitions/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace Frends.Jira.DeleteIssue.Definitions;
+
+/// <summary>
+/// Task's result.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// Operation complete without errors.
+    /// </summary>
+    /// <example>true</example>
+    public bool Success { get; private set; }
+
+    /// <summary>
+    /// Request content as JSON string.
+    /// </summary>
+    /// <example>"{\"expand\":\"renderedFields,names,schema,operations,editmeta,changelog,versionedRepresentations\",\"id\":\"151894\",..."</example>
+    public string Data { get; private set; }
+
+    /// <summary>
+    /// Error message.
+    /// </summary>
+    /// <example>An error occured...</example>
+    public string ErrorMessage { get; private set; }
+
+    internal Result(bool success, string data, string errorMessage)
+    {
+        Success = success;
+        Data = data;
+        ErrorMessage = errorMessage;
+    }
+}
